a1131f02b47fb7c874ff598ba02184471db859aa
StackHustla <153138763+StackHustla@users.noreply.github.com>
2024-03-05 15:50:29 -0600

    Slot Machine functionality
    Functioning normally. Need to add function to see if you're a winner.

---
+++ project.js
@@ -0,0 +1,131 @@
+/* 1. deposit some money */
+/* 2. Determine number of lines to bet on */
+/* 3. Collect a bet amount */
+/* 4. Spin slot machine */
+/* 5. Check if user won or Take their money */
+/* 6. Play again */
+
+
+/* classic way to call a function - delete later*/
+
+/* function deposit () {
+return 1
+}
+ */
+
+/* prompts user to give input */
+const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {/* revisit this at 29:33 so that I can understand it more. Or use chatgpt */
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+/* new (better) way to call a function */
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again or Get your money up!")
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again!")
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again!")
+        } else {
+            return numberBet;
+        }
+    }
+};
+
+/* generating an array */
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != rows.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+
+console.log(reels);
+console.log(rows);
+console.log("$" + balance);
+console.log("Betting lines: " + numberOfLines);
